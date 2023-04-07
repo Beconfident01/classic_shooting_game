8bf8e69940471015f5a090576fe28f4f869fe587
Beconfident01 <hom0726@naver.com>
2023-04-07 14:41:56 +0900

    기본 클래스 세팅

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+        Game game = new Game();
+        Player player = new Player();
+
+        System.out.println("게임 종료!");
+        System.out.println("점수: " + game.getScore());
+    }
+}
+
+
+
+
+
+
